90e74a659384ed4e668c4dfcc106a812f8125a37
Christian Jerrin Thomas <45801030+JDChristo@users.noreply.github.com>
2024-09-11 11:24:24 +0530

    log

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 document.getElementById("clickable").addEventListener("click", function(event) {
     var divHeight = this.offsetHeight;
+    console.log(divHeight);
     var clickY = event.offsetY;
     
     if (clickY < divHeight / 2) {
-      window.location.href = "https://link1.com";
+      window.location.href = "https://google.com";
     } else {
       window.location.href = "https://link2.com";
     }
